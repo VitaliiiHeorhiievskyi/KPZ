748b81bcc5d6677c1c0e897139c251fb2d38952e
VitaliiHeorhiievskyi <91679829+VitaliiiHeorhiievskyi@users.noreply.github.com>
2023-12-10 18:05:17 +0200

    Add tests

---
+++ WebApi.UnitTest/AuthenticationServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using PatientHealth.DataBase;
+using WebApi.Interfaces;
+using WebApi.Models;
+using WebApi.Services;
+using WebApi.ViewModels;
+using Xunit;
+
+namespace WebApi.Services.Tests
+{
+    public class AuthenticationServiceTests
+    {
+        [Fact]
+        public async Task Login_ValidCredentials_ReturnsPatient()
+        {
+            // Arrange
+            var loginRequest = new LoginRequest { Email = "john.doe@example.com", Password = "password123" };
+
+            var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "InMemory_Test_Database")
+                .Options;
+
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var authenticationService = new AuthenticationService(dbContext);
+
+            var existingPatient = new Patient
+            {
+                Id = new Guid(),
+                Email = "john.doe@example.com",
+                Password = "password123",
+                Address = "asd",
+                FirstName = "asd",
+                LastName = "asd",
+                PhoneNumber = "1234567890",
+            };
+
+            dbContext.Patients.Add(existingPatient);
+            dbContext.SaveChanges();
+
+            // Act
+            var result = await authenticationService.Login(loginRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(existingPatient, result);
+        }
+
+        [Fact]
+        public async Task Login_InvalidCredentials_ReturnsNull()
+        {
+            // Arrange
+            var loginRequest = new LoginRequest { Email = "john.doe@example.com", Password = "wrongPassword" };
+
+            var dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "InMemory_Test_Database")
+                .Options;
+
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var authenticationService = new AuthenticationService(dbContext);
+
+            // Act
+            var result = await authenticationService.Login(loginRequest);
+
+            // Assert
+            Assert.Null(result);
+        }
+    }
+}
---
+++ WebApi.UnitTest/PatientServiceTests.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using PatientHealth.DataBase;
+using WebApi.Interfaces;
+using WebApi.Models;
+using Xunit;
+
+namespace WebApi.Services.Tests
+{
+    public class PatientServiceTests
+    {
+        private DbContextOptions<AppDbContext> CreateInMemoryDbContextOptions(string databaseName)
+        {
+            return new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: databaseName)
+                .Options;
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ReturnsPatient()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var dbContextOptions = CreateInMemoryDbContextOptions("InMemory_Test_Database");
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var patientService = new PatientService(dbContext);
+
+            var patient = new Patient
+            {
+                Id = id,
+                FirstName = "John",
+                LastName = "Doe",
+                Address = "asd",
+                Email = "asd",
+                Password = "ads",
+                Sex = Sex.Male,
+                BirthDate = DateTime.Now,
+                PhoneNumber = "asd"
+            };
+            dbContext.Patients.Add(patient);
+            dbContext.SaveChanges();
+
+            // Act
+            var result = await patientService.GetByIdAsync(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(id, result?.Id);
+            Assert.Equal("John", result?.FirstName);
+            Assert.Equal("Doe", result?.LastName);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_ReturnsNullForNonExistentPatient()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var dbContextOptions = CreateInMemoryDbContextOptions("InMemory_Test_Database");
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var patientService = new PatientService(dbContext);
+
+            // Act
+            var result = await patientService.GetByIdAsync(id);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_UpdatesPatient()
+        {
+            // Arrange
+            var dbContextOptions = CreateInMemoryDbContextOptions("InMemory_Test_Database");
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var patientService = new PatientService(dbContext);
+
+            var existingPatient = new Patient
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com",
+                Password = "oldPassword",
+                BirthDate = new DateTime(1990, 1, 1),
+                Address = "123 Main St",
+                PhoneNumber = "555-1234",
+                Sex = Sex.Male
+            };
+
+            dbContext.Patients.Add(existingPatient);
+            dbContext.SaveChanges();
+
+            var updatedPatient = new Patient
+            {
+                Id = existingPatient.Id,
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "jane.doe@example.com",
+                Password = "newPassword",
+                BirthDate = new DateTime(1995, 2, 2),
+                Address = "456 Oak St",
+                PhoneNumber = "555-5678",
+                Sex = Sex.Female
+            };
+
+            // Act
+            await patientService.UpdateAsync(updatedPatient);
+
+            // Assert
+            var updatedPatientFromDb = dbContext.Patients.FirstOrDefault(p => p.Id == updatedPatient.Id);
+            Assert.NotNull(updatedPatientFromDb);
+            Assert.Equal("Jane", updatedPatientFromDb.FirstName);
+            Assert.Equal("Doe", updatedPatientFromDb.LastName);
+            Assert.Equal("jane.doe@example.com", updatedPatientFromDb.Email);
+            Assert.Equal("newPassword", updatedPatientFromDb.Password);
+            Assert.Equal(new DateTime(1995, 2, 2), updatedPatientFromDb.BirthDate);
+            Assert.Equal("456 Oak St", updatedPatientFromDb.Address);
+            Assert.Equal("555-5678", updatedPatientFromDb.PhoneNumber);
+            Assert.Equal(Sex.Female, updatedPatientFromDb.Sex);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_DoesNotUpdateForNonExistentPatient()
+        {
+            // Arrange
+            var dbContextOptions = CreateInMemoryDbContextOptions("InMemory_Test_Database");
+            using var dbContext = new AppDbContext(dbContextOptions);
+            var patientService = new PatientService(dbContext);
+
+            var nonExistentPatient = new Patient
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Non",
+                LastName = "Existent"
+            };
+
+            // Act
+            await patientService.UpdateAsync(nonExistentPatient);
+
+            // Assert
+            var nonExistentPatientFromDb = dbContext.Patients.FirstOrDefault(p => p.Id == nonExistentPatient.Id);
+            Assert.Null(nonExistentPatientFromDb);
+        }
+    }
+}
